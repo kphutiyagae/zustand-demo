77dcf0cfd95278eedc121999829e91f37c23f59a
kphutiyagae <kopanophutiyagae@gmail.com>
2024-02-17 13:42:54 +0200

    feat(none): Added increment and decrement functions to the App.tsx and works.

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import {useCounterStore} from "./store/store.ts";
 
 function App() {
   const count = useCounterStore((state) => state.count) //This is essentially a selector for a piece of state.
+  const increment = useCounterStore(state => state.increment);
+  const decrement = useCounterStore(state => state.decrement);
 
   return (
     <>
     <div>
-        The count is: {count}
+      <p>The count is: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
     </>
   )
