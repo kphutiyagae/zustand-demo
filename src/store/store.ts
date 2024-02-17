1b295cd3676c147565ac1ea0f78fbfca536e2c74
kphutiyagae <kopanophutiyagae@gmail.com>
2024-02-17 13:35:22 +0200

    feat(none): Added increment and decrement functions to the counter store.

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,8 +2,16 @@ import {create} from "zustand";
 
 type CounterStore = {
     count: number;
+    increment: () => void;
+    decrement: () => void;
 }
 
-export const useCounterStore = create<CounterStore>( () => ({
-    count: 0
+export const useCounterStore = create<CounterStore>( (set) => ({
+    count: 0,
+    increment: () => {
+        set({count: 1}) //New Values being set to state.
+    },
+    decrement: () => {
+        set({count: -1})
+    }
 }))
